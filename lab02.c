3b9c1395e9ffe30e0e286ba9dd3ca9e512716fe4
Mauriciobh05 <153849966+Mauriciobh05@users.noreply.github.com>
2025-11-11 11:18:26 -0500

    Update lab02.c

--- lab02.c
+++ lab02.c
@@ -16,42 +16,41 @@
 #include <sys/types.h>
 
 int main() {
-    int er;
-    char nomdir[100], nomfich[100];
-    struct stat atr;
-    DIR *d;
-    struct dirent *rd1;
-    time_t fecha;
+    int er;  // Se crea esta variable para guardar el resultado de la función stat
+    char nomdir[100], nomfich[100]; // se crean estos arreglos para el nombre del directorio y del archivo
+    struct stat atr; // Se crea esta estructura donde se almacenan los atributos del archivo
+    DIR *d;  // Se crea un puntero al directorio o archivo 
+    struct dirent *rd1; // Se crea una estructura para leer las entradas del directorio o archivo 
+    time_t fecha; // Se crea variable para guardar la fecha y hora actual del sistema
 
-    printf("Nombre del directorio:\n");
-    fgets(nomdir, sizeof(nomdir), stdin);
+    printf("Nombre del directorio:\n"); // Se le solicita al usuario el nombre del directorio o archivo que desea analizar
+    fgets(nomdir, sizeof(nomdir), stdin); // Lee el nombre del archivo o directorio desde el teclado 
 
-    // Quitar salto de línea al final
-    nomdir[strlen(nomdir) - 1] = '\0';
-    fecha = time(&fecha);
+    nomdir[strlen(nomdir) - 1] = '\0'; 
+    fecha = time(&fecha); // Se guarda la fecha y hora 
 
-    // Intentar abrir el directorio
+    // Se intenta abrir el directorio
     if ((d = opendir(nomdir)) == NULL) {
-        printf("No existe ese directorio.\n");
-        return -1;
+        printf("No existe ese directorio.\n"); // si no se puede abrir imprime este mensaje 
+        return -1; // retorna error 
     }
 
     // Leer cada entrada del directorio
     while ((rd1 = readdir(d)) != NULL) {
         if (strcmp(rd1->d_name, ".") != 0 && strcmp(rd1->d_name, "..") != 0) {
-            strcpy(nomfich, nomdir);
+            strcpy(nomfich, nomdir); // Copia el nombre del directorio
             strcat(nomfich, "/");
-            strcat(nomfich, rd1->d_name);
+            strcat(nomfich, rd1->d_name); // Añade el nombre del archivo actual
 
-            printf("\nFichero: %s\n", nomfich);
+            printf("\nFichero: %s\n", nomfich); // Muestra el nombre completo del fichero actual 
             er = stat(nomfich, &atr);
             if (er != 0) {
                 perror("Error obteniendo información del archivo");
-                continue;
+                continue; // sigue al siguiente archivo 
             }
 
-            printf("Modo: %lo\n", (unsigned long)atr.st_mode);
-            if ((atr.st_mode & 0400) != 0)
+            printf("Modo: %lo\n", (unsigned long)atr.st_mode);  // Se muestra los permisos y tipo de archivo en formato ls -l
+            if ((atr.st_mode & 0400) != 0) // // Se verifica si el propietario tiene permiso de lectura
                 printf("→ Permiso R para propietario\n");
             else
                 printf("→ No permiso R para propietario\n");
